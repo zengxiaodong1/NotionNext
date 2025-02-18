f6b83546eacfae12f35aaf0cde095f7499dc50e1
zengxiaodong1 <106065525+zengxiaodong1@users.noreply.github.com>
2025-02-18 21:06:18 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -8,7 +8,7 @@ export default function PoweredBy(props) {
   return (
     <div
       className={`gap-x-1 flex flex-wrap text-sm font-serif ${props.className || ''}`}>
-      <span>Powered by</span>
+      <span>备案信息：</span>
       <a
         href='https://beian.miit.gov.cn/'
         className='underline justify-start'>
